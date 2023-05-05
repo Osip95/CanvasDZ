ab7799c7aeb94200bf33136dfed7eb365540e2cd
os <osss151095@gmail>
2023-05-05 19:51:18 +0300

    добавил элемент выбора размера кисти на панель инструментов

--- app/src/main/java/com/example/canvas/TOOLS.kt
+++ app/src/main/java/com/example/canvas/TOOLS.kt
@@ -2,12 +2,12 @@ package com.example.canvas
 
 import androidx.annotation.DrawableRes
 
-enum class TOOLS(
+enum class TOOLS( // перечесление кнопок панели управления
     @DrawableRes
     val value: Int
 ) {
     NORMAL(R.drawable.ic_horizontal_line),
     DASH(R.drawable.ic_dashed_line),
-    //    SIZE(R.drawable.ic_baseline_brightness_1_24),
+    SIZE(R.drawable.ic_baseline_brightness_1_24),
     PALETTE(R.drawable.ic_baseline_brightness_1_24)
 }
